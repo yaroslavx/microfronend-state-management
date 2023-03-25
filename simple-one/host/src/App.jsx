a8937688db39c60b2764565e12708b9b6c2d6d87
Yaroslav Yakupov <yakupov.yaroslav@icloud.com>
2023-03-25 11:13:53 +0300

    Completed simple state management

--- simple-one/host/src/App.jsx
+++ simple-one/host/src/App.jsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
 import { Header } from "nav/Header";
 
-const App = () => (
-  <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <Header />
-    <div>Name: host</div>
-  </div>
-);
+const App = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <div className="mt-10 text-3xl mx-auto max-w-6xl">
+      <Header count={count} onClear={() => setCount(0)} />
+      <div>Name: host</div>
+      <div>count {count}</div>
+      <button
+        onClick={() => setCount(count + 1)}
+        className="bg-indigo-400 text-white font-bold py-2 px-4 rounded"
+      >
+        +1
+      </button>
+    </div>
+  );
+};
 ReactDOM.render(<App />, document.getElementById("app"));
--- simple-one/nav/src/Header.jsx
+++ simple-one/nav/src/Header.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
-export const Header = () => {
-    return (
-        <header className='bg-blue-400 text-white font-bold text-3xl p-5 flex'>
-            <div>Header</div>
-        </header>
-    )
-}
+import React from "react";
+export const Header = ({ count, onClear }) => {
+  return (
+    <header className="bg-blue-400 text-white font-bold text-3xl p-5 flex gap-3">
+      <div className="flex-grow">Header</div>
+      <div>
+        count {count}
+        <button
+          onClick={onClear}
+          className="bg-red-400 text-white font-bold py-2 px-4 rounded"
+        >
+          clear count
+        </button>
+      </div>
+    </header>
+  );
+};
