97f1b961a02d3d4592172335595bdb869b7306aa
Yaroslav Yakupov <yakupov.yaroslav@icloud.com>
2023-03-25 10:56:26 +0300

    Implemented basic state management: nothing interesting

---
+++ simple-one/nav/src/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+export const Header = () => {
+    return (
+        <header className='bg-blue-400 text-white font-bold text-3xl p-5 flex'>
+            <div>Header</div>
+        </header>
+    )
+}
